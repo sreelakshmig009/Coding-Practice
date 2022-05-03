7b36e1d72b20ff31b49cec9341a5fe9bbdaf371d
Sreelakshmi G <sreelakshmig009@gmail.com>
2022-05-03 16:16:46 +0530

    Create arrays-1.py

---
+++ arrays-1.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/find-the-duplicate-number/
+count = {}
+for num in nums:
+  if num not in nums:
+    count[num] = 1
+  else:
+    count[num] += 1
+ for key,value in count.items():
+  if value>1:
+    return key
